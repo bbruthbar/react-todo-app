a01f80811c4f69041fbc983be51953dc5ad1ece6
Ruth Cirillo <ruthcirillo@Ruths-MBP.fios-router.home>
2019-05-05 20:59:16 -0400

    Updates

---
+++ precache-manifest.6e4b86516f885104922f4471debe868e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be940bcbef34459d0865693a8c6b7d4f",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "d929e898c486bce86cbf",
+    "url": "/react-todo-app/static/css/main.ad38bcf9.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-todo-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d929e898c486bce86cbf",
+    "url": "/react-todo-app/static/js/main.512de409.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
